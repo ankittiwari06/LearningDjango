f34fe4b8b6b7416d413117fb63bf86363be95cae
Ankit Tiwari <ankittiwari0693@gmail.com>
2016-11-28 11:46:57 +0530

    Update python_samplecmd2.py

--- python_samplecmd2.py
+++ python_samplecmd2.py
@@ -1,27 +1,40 @@
 from cmd import Cmd
 import re
+import random
 
 class Mycmd(Cmd):
 
     
     def do_fileopen(self, args):
-        my_file = open("File.txt",'r')
+        my_file = open("/home/ankit/Python_practise/file1.txt",'r')
         file_contents = my_file.read()
         print(file_contents)
+        #fileName = input("File Name to open : ")
+      #  with open(file, 'r') as f:
+       #     return do_fileopen(f)
+    #print(do_fileopen(fileName))
+        
+     
+#    def do_filename(file):
+ #      my_file = open(file, 'r')
+  #     return my_file
+   # file=input("File : ")
+    #print(do_filename(file))
+   
 
     def do_wordcount(self, args):
-        my_file = open("File.txt",'r')
+        my_file = open("/home/ankit/Python_practise/file1.txt",'r')
         file_contents = my_file.read()
         values = file_contents.split()
         counts = []
         for word in values:
             counts.append(values.count(word))
-            print(list(zip(values, counts)))
+        print(list(zip(values, counts)))
 
     def do_search(self, args):
         total = 0
         ip = input("Please enter the word you want to find in the file : ")
-        with open('File.txt') as f :
+        with open('/home/ankit/Python_practise/File.txt') as f :
             for line in f:
                 finded=line.find(ip)
                 if finded != -1 and finded != 0 :
@@ -29,7 +42,7 @@ class Mycmd(Cmd):
         print(total)
     
     def do_show(self, args):
-        my_file = open("File.txt",'r')
+        my_file = open("/home/ankit/Python_practise/File.txt",'r')
         file_contents = my_file.read()
         values = file_contents.split()
         counts = []
@@ -39,7 +52,17 @@ class Mycmd(Cmd):
         for element in filelist :
             p = re.compile(r'^t')
             m = p.search("this is the and the tiger is a byth")
-        print(m)
+        
+
+    def do_randomWord(self, args):
+        my_file = open("/home/ankit/Python_practise/file1.txt",'r')
+        file_contents = my_file.read()
+        values = file_contents.split()
+        #counts = []
+        #for word in values:
+        #    counts.append(values.count(word))
+        print(random.choice(values))
+
 
     def do_quit(self, args):
         print("Quitting.")
@@ -47,5 +70,5 @@ class Mycmd(Cmd):
 
 if __name__ == '__main__':
     values = Mycmd()
-    values.prompt = 'ankit '
-    values.cmdloop('Starting cmd...')
+    values.prompt = '>>'
+    values.cmdloop('Starting cmd Prompt...')

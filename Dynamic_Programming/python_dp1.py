698f8a059d6fc0d10ed9b9cc6418d4d449cc1634
Ankit Tiwari <ankittiwari0693@gmail.com>
2016-11-29 16:22:24 +0530

    Update python_dp1.py

--- Dynamic_Programming/python_dp1.py
+++ Dynamic_Programming/python_dp1.py
@@ -1,4 +1,3 @@
-#Question:
 #Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
 #between 1000 and 3000 (both included).
 #The numbers obtained should be printed in a comma-separated sequence on a single line.
